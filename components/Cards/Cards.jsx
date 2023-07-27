a232ad9b98876cae78cebad9243db02b9f79760a
Ksel Ivan <Kselivan98@gmail.com>
2023-07-27 14:23:27 +0500

    make test card on main page

--- components/Cards/Cards.jsx
+++ components/Cards/Cards.jsx
@@ -1,3 +1,26 @@
-const Cards = () => {
-    return 
+import styles from "./Cards.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+
+const Cards = ({ cards }) => {
+    return (
+        <div className={styles.cards}>
+            {cards.map((item) => {
+                return (
+                    <div className={styles.cards__card}>
+                        <Image 
+                            src='/jpg/test1.jpg'
+                            height={108}
+                            width={108}
+                            alt='photo'
+                        />
+                        <h1>{item.title}</h1>
+                        <p>{item.content}</p>  
+                    </div>
+                )
+            })}
+        </div> 
+    ) 
 }
+
+export default Cards;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 import { Person } from "../Service/PageServive";
 import Header from "../components/Header/Header";
+import Cards from "../components/Cards/Cards";
 
 export default function Index (props) {
     const [cards, setCards] = useState(props.data.person);
 
-    console.log('cards', cards);
-
     return (
         <div>
             <Header />
+            <Cards 
+                cards={cards}
+            />
         </div>
     )
 }
